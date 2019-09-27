2ba90213a10785b0f6b92ddda55657af557d423a
Pablo Cianferoni <53634806+pcianferoni@users.noreply.github.com>
2019-09-27 18:59:27 -0300

    Update product.js
    2

--- server/models/product.js
+++ server/models/product.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var productSchema = new Schema({
     name: { type: String, required: [true, 'name is required'] },
-    unitprice: { type: Number, required: [true, 'unit price is required'] },
+    unitPrice: { type: Number, required: [true, 'unit price is required'] },
     description: { type: String, required: false },
     img: { type: String, required: false },
     available: { type: Boolean, required: true, default: true },
